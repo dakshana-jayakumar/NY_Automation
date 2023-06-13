6d2d865207da2e54dfd945854e990f898aeb980d
dakshana.j@juspay.in <dakshana.j@juspay.in>
2023-06-13 22:05:43 +0530

    allure reports

--- src/test/java/NYAutomation/AutomationFlow.java
+++ src/test/java/NYAutomation/AutomationFlow.java
@@ -3,54 +3,50 @@ package NYAutomation;
 
 import base.BaseClass;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.MediaEntityBuilder;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.security.GeneralSecurityException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.Duration;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.TakesScreenshot;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.Listeners;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
+import java.io.ByteArrayInputStream;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
 
 @Listeners(NYAutomation.AutomationFlow.class)
-public class AutomationFlow extends BaseClass implements ITestListener{
+public class AutomationFlow extends BaseClass implements ITestListener {
 
     private String RideOtp = "";
-    private static ExtentTest test; // Updated variable declaration
-    ExtentReports extentReports = setupReport();
 
     @Test
     /* Creating a method for overall flow of the applications */
-    private void flow() throws InterruptedException, IOException, GeneralSecurityException {
-        // Fetch test data from Google Sheet
-    	TestDataReader.fetchTabNames();
+    public void flow() throws InterruptedException, IOException {
+    	/* Fetch test data from Google Sheet */
+        TestDataReader.fetchTabNames();
         String[][] testData = TestDataReader.testData.toArray(new String[0][0]);
-        
+
         boolean userFlag = true;
         boolean driverFlag = true;
 
@@ -61,18 +57,14 @@ public class AutomationFlow extends BaseClass implements ITestListener{
             String xpath = actionParameter[3];
             String sendKeysValue = actionParameter[4];
             boolean isUser = "user".equals(actionParameter[5]);
-  
-            if(userFlag && isUser) {
-            	userFlag = false;
-            	setup(isUser);
-            }
-            else if(driverFlag && !isUser) {
-            	driverFlag = false;
-            	setup(isUser);
-            }
 
-         /* Create a new test in the report */
-            test = extentReports.createTest(screen, state);
+            if (userFlag && isUser) {
+                userFlag = false;
+                setup(isUser);
+            } else if (driverFlag && !isUser) {
+                driverFlag = false;
+                setup(isUser);
+            }
 
             checkCase(testCase, screen, state, xpath, sendKeysValue, isUser);
             System.out.println("screen: " + screen + " | state: " + screen + " | XPath: " + xpath + " | SendKeys Value: " + sendKeysValue);
@@ -81,15 +73,15 @@ public class AutomationFlow extends BaseClass implements ITestListener{
 
     /* method used to code all the types of functions to be handled */
     public void checkCase(String testCase, String screen, String state, String xpath, String sendKeysValue, boolean isUser) throws InterruptedException {
-        /* Creating a wait object to wait for the user or driver */
+    	/* Creating a wait object to wait for the user or driver */
         Wait<AndroidDriver> wait = new FluentWait<>(isUser ? user : driver)
-                .withTimeout(Duration.ofSeconds(30)) /* Set the timeout duration to 30 seconds */
-                .pollingEvery(Duration.ofMillis(1000)) /* Set the polling interval to 1000 milliseconds */
-                .ignoring(Exception.class); /* Ignore exceptions during the wait */
+                .withTimeout(Duration.ofSeconds(20))
+                .pollingEvery(Duration.ofMillis(1000))
+                .ignoring(Exception.class);
 
         if (state.equals("Fetch Otp")) {
-            /* Fetching OTP digits */
-        	for (int i = 1; i <= 4; i++) {
+        	/* Fetching OTP digits */
+            for (int i = 1; i <= 4; i++) {
                 RideOtp = RideOtp + wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath + "/../android.widget.LinearLayout/android.widget.LinearLayout[" + i + "]/android.widget.TextView"))).getText();
             }
             System.out.println("Ride Otp = " + RideOtp);
@@ -104,144 +96,143 @@ public class AutomationFlow extends BaseClass implements ITestListener{
             }
             System.out.println("Ride Otp = " + RideOtp);
             
-        	return;
+            return;
         }
-        
-         /* if any specific cases have to be performed */
+
+        /* if any specific cases have to be performed */
         if ("Choose Language".equals(screen) && !"Update Language".equals(screen)) {
-                //     Wait for the element to be visible before scrolling
-                //    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
-        
-                    // Scroll to the desired text
-                    scrollToText("Tamil");
+            scrollToText("Tamil");
         }
         if (("Stats Dashboard".equals(screen)) || ("Logout Section".equals(screen))) {
             Thread.sleep(5000);
-            // Add handling of Android back key here
             (isUser ? user : driver).pressKey(new KeyEvent(AndroidKey.BACK));
             return;
         }
 
         /* Button layout locator */
         By buttonLayoutLocator = By.xpath(xpath);
-
         /* Performing action based on input */
         performAction(wait, buttonLayoutLocator, sendKeysValue);
     }
 
     /**
-     * Performs the action based on the provided parameters.
-     *
-     * @param wait           Wait object for waiting conditions
-     * @param buttonLayoutLocator Locator for the button layout
-     * @param sendKeysValue  Value to be sent as keys (if not empty)
-    **/
-    public void performAction(Wait<AndroidDriver> wait, By buttonLayoutLocator, String sendKeysValue) {
-        if (!sendKeysValue.isEmpty()) {
-            /* perform send keys action to the element */
-            wait.until(ExpectedConditions.visibilityOfElementLocated(buttonLayoutLocator)).sendKeys(sendKeysValue);
-        } else {
-            /* perform click action on the element */
-            wait.until(ExpectedConditions.elementToBeClickable(buttonLayoutLocator)).click();
-        }
-    }
-    
-    /* Scrolls to the specified text */
-    public void scrollToText(String text) {
+   * Performs the action based on the provided parameters.
+   *
+   * @param wait           Wait object for waiting conditions
+   * @param buttonLayoutLocator Locator for the button layout
+   * @param sendKeysValue  Value to be sent as keys (if not empty)
+  **/
+  public void performAction(Wait<AndroidDriver> wait, By buttonLayoutLocator, String sendKeysValue) {
+      if (!sendKeysValue.isEmpty()) {
+          /* perform send keys action to the element */
+          wait.until(ExpectedConditions.visibilityOfElementLocated(buttonLayoutLocator)).sendKeys(sendKeysValue);
+      } else {
+          /* perform click action on the element */
+          wait.until(ExpectedConditions.elementToBeClickable(buttonLayoutLocator)).click();
+      }
+  }
+  
+  /* Scrolls to the specified text */
+  public void scrollToText(String text) {
 
 		driver.findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0))"
 				+ ".scrollIntoView(new UiSelector()" + ".textMatches(\"" + text + "\").instance(0))"));
 	}
-          
-        
-    /* Captures a screenshot and replaces the existing screenshot with the latest one */
-    private String getScreenshotPathForUser(String testCase) {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HH");
-        String timestamp = now.format(formatter);
-
-        if (user != null) {
-	        File userSrcFile = ((TakesScreenshot) user).getScreenshotAs(OutputType.FILE);
-            String userDestFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
-                    + File.separator + "java" + File.separator + "NYAutomation" + File.separator + "screenshots"
-                    + File.separator + testCase + "_user_" + timestamp + ".png";
-            saveScreenshot(userSrcFile, userDestFilePath);
-            return userDestFilePath;
-        	} else {
-	        	System.out.println("User is not initialized!");
-	            return null;
-	        }
-        }
-
-    public String getScreenshotPathForDriver(String testCase) {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HH");
-        String timestamp = now.format(formatter);
-
-        if (driver != null) {
-            File driverSrcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            String driverDestFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
-                    + File.separator + "java" + File.separator + "NYAutomation" + File.separator + "screenshots"
-                    + File.separator + testCase + "_driver_" + timestamp + ".png";
-            saveScreenshot(driverSrcFile, driverDestFilePath);
-
-            return driverDestFilePath;
-        } else {
-            System.out.println("Driver is not initialized!");
-            return null;
-        }
+    
+    
+    /* Method saves the screenshot as an attachment in Allure report */
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] saveScreenshot(byte[] screenshot) {
+        return screenshot;
     }
 
-    public void saveScreenshot(File srcFile, String destFilePath) {
-        // Replace the existing screenshot file with the latest one
-        try {
-            Files.copy(srcFile.toPath(), new File(destFilePath).toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    /* Method retrieves the name of the test method that is currently being executed */
+    private static String getTestMethodName(ITestResult iTestResult) {
+        return iTestResult.getMethod().getConstructorOrMethod().getName();
     }
-        
-        
-    /* Setup ExtentReports */
-    public ExtentReports setupReport() {
-        String path = System.getProperty("user.dir") + File.separator + File.separator + "src" + File.separator + "main"
-                + File.separator + "java" + File.separator + "NYAutomation" + File.separator + "Reports" + File.separator + "report_" + System.currentTimeMillis() + ".html";
+    /* Method is executed at the start of the test suite and logs the name of the test suite */
+    @Override
+    public void onStart(ITestContext iTestContext) {
+        log("Test Suite started: " + iTestContext.getName());
+        iTestContext.setAttribute("WebDriver", BaseClass.getDriver());
+    }
 
-        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
-        reporter.config().setReportName("Namma Yatri Test Report");
-        reporter.config().setDocumentTitle("Test Results");
+    /* Method is executed at the end of the test suite and logs the name of the test suite */
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+        log("Test Suite finished: " + iTestContext.getName());
+    }
 
-        extentReports = new ExtentReports();
-        extentReports.attachReporter(reporter);
-        return extentReports;
+    /* Method is executed at the start of each test case and logs the name of the test case */
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        log("Test case started: " + getTestMethodName(iTestResult));
+    }
+
+    /* Method is executed when a test case succeeds and logs the name of the test case */
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        log("Test case succeeded: " + getTestMethodName(iTestResult));
     }
     
+    /* Method is executed when a test case fails. It logs the name of the failed test case and captures a screenshot using the WebDriver instance and attaches it to the Allure report */
     @Override
-  public void onTestSuccess(ITestResult result) {
-          test.log(Status.PASS, "Test Passed");
-      }
-    
-    @Override
-    public void onTestFailure(ITestResult result) {
-        test.fail(result.getThrowable());
-
-        String userScreenshotPath = getScreenshotPathForUser(result.getMethod().getMethodName());
-        String driverScreenshotPath = getScreenshotPathForDriver(result.getMethod().getMethodName());
-        
-        if (userScreenshotPath != null && !userScreenshotPath.isEmpty()) {
-            test.info("User Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(userScreenshotPath).build());
-        } 
-        
-
-        if (driverScreenshotPath != null && !driverScreenshotPath.isEmpty()) {
-            test.info("Driver Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(driverScreenshotPath).build());
+    public void onTestFailure(ITestResult iTestResult) {
+        log("Test case failed: " + getTestMethodName(iTestResult));
+        Object testClass = iTestResult.getInstance();
+        WebDriver driver = BaseClass.getDriver();
+        if (driver instanceof WebDriver) {
+            log("Capturing screenshot for failed test case: " + getTestMethodName(iTestResult));
+            try {
+                captureScreenshotAndAttach(driver, getTestMethodName(iTestResult));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+    
+    /* Method is executed when a test case is skipped and logs the name of the skipped test case */
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        log("Test case skipped: " + getTestMethodName(iTestResult));
+    }
 
-    /* Tear down ExtentReports */
+    /* Method is executed when a test case fails within the defined success percentage and logs the name of the failed test case */
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+        log("Test case failed but within success percentage: " + getTestMethodName(iTestResult));
+    }
+
+    /* Method captures a screenshot using the WebDriver instance and returns it as a byte array */
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] captureScreenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    /* Method logs a message using Allure step and also prints it to the console */
+    @Step("{0}")
+    public void log(String message) {
+        Allure.step(message);
+        System.out.println(message);
+    }
+
+    /* Method captures a screenshot using the WebDriver instance, converts it to a byte array, and attaches it to the Allure report with a specified name */
+    public void captureScreenshotAndAttach(WebDriver driver, String screenshotName) throws IOException {
+        byte[] screenshot = captureScreenshot(driver);
+        Allure.addAttachment(screenshotName, new ByteArrayInputStream(screenshot));
+    }
+
+    /* Method reads the byte array from the specified screenshot file and attaches it to the Allure report with a specified name */
+    @Attachment(value = "{0}", type = "image/png")
+    public byte[] attachScreenshot(String attachmentName, String screenshotPath) throws IOException {
+        return Files.readAllBytes(Paths.get(screenshotPath));
+    }
+
+    /* Method is executed after the test suite finishes and quits the WebDriver instances for both user and driver */
     @AfterSuite
-    public void tearDownReport() {
-    	extentReports.flush();
+    public void tearDown() {
+    	user.quit();
+    	driver.quit();
     }
     
 }
